eacdfcf49b0aec31205b4621a9adff32a42bdcc7
albertocruz6 <alberto.cruz6@upr.edu>
2021-03-07 19:08:04 -0400

    First Tests in Raspberry GPIO

---
+++ pin_ex_1.py
@@ -0,0 +1,17 @@
+from gpiozero import LED
+from time import sleep
+from signal import pause
+
+red = LED(17)
+
+'''
+while True:
+    red.on()
+    sleep(1)
+    red.off()
+    sleep(1)
+
+'''
+
+red.blink()
+pause()
